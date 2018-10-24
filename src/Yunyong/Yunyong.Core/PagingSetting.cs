2e393efbfc20fc0938b7e738d24106850bf8c706
陈勇兵 <xibeiwind@126.com>
2018-10-24 17:04:16 +0800

    添加默认页面大小配置功能

---
+++ src/Yunyong/Yunyong.Core/PagingSetting.cs
@@ -0,0 +1,20 @@
+﻿namespace Yunyong.Core
+{
+    public static class PagingSetting
+    {
+        private static int _defaultPageSize = 10;
+
+        public static int DefaultPageSize
+        {
+            get => _defaultPageSize;
+            set
+            {
+                if (value <= 0)
+                {
+                    value = 10;
+                }
+                _defaultPageSize = value;
+            }
+        }
+    }
+}
